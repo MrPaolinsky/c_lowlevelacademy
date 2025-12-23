c5558d66f1bb83ee049683a73083fe662d76535d
MrPaolinsky <paolinskypdtn@proton.me>
2025-12-23 11:41:05 -0500

    socket binding example

--- sockets.c
+++ sockets.c
@@ -1,10 +1,21 @@
+#include <arpa/inet.h>
+#include <netinet/in.h>
 #include <stdio.h>
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <unistd.h>
 
 int
 main ()
 {
+    struct sockaddr_in serverInfo = { 0 };
+    struct sockaddr_in clientInfo = { 0 };
+    int clientSize = 0;
+
+    serverInfo.sin_family = AF_INET;
+    serverInfo.sin_addr.s_addr = 0;
+    serverInfo.sin_port = htons (5555); // Network endian needed here.
+
     // Sockets are file descriptors
     // Sockets are endpoints for communication
     // There are many types of sockets, the one on this example is a
@@ -20,4 +31,29 @@ main ()
     printf ("%d\n", fd); // This file descriptor represents an endpoint for
                          // communication, can be passed to
     // future function with sys calls to do things over the network
+    //
+    // bind
+    if (bind (fd, (struct sockaddr *)&serverInfo, sizeof (serverInfo)) == -1)
+        {
+            perror ("bind");
+            close (fd);
+            return -1;
+        }
+    // listen
+    if (listen (fd, 0) == -1)
+        {
+            perror ("listen");
+            close (fd);
+            return -1;
+        }
+    // accept
+    int cfd = accept (fd, (struct sockaddr *)&clientInfo, &clientSize);
+    if (cfd == -1)
+        {
+            perror ("accept");
+            close (fd);
+            return -1;
+        }
+
+    close (cfd);
 }
